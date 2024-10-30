1b3215b696e8a9179b231aeb857ca629e5c14ec9
iTop1337 <lucas.adcost@gmail.com>
2024-10-29 23:31:48 -0300

    none to None

--- chatbot_project/chatbot/vector_database.py
+++ chatbot_project/chatbot/vector_database.py
@@ -3,7 +3,7 @@ from langchain import Weaviate
 def inialize_vector_store():
         vector_store = Weaviate(
                 url="https://localhost:8080",
-                auth_client_secret=Noneone
+                auth_client_secret=None
         )
 
         return vector_store
